d2bfe6e6a41c0295533a81c316f1903dca692d3e
Jim VanOosten <jimvo@us.ibm.com>
2020-05-03 23:41:40 -0400

    update flask to use port 8080

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ def predict():
         return render_template('index.html', prediction_text='Prediction Err !!!')
 
 if __name__ == "__main__":
-    app.run(host= '0.0.0.0', debug=True)
+    app.run(host= '0.0.0.0', port=8080, debug=True)
